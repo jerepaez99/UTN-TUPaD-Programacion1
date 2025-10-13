f84a897a976f150029fab689edb96566c9c57367
jerepaez99 <jerepaez99@gmail.com>
2025-10-13 20:52:02 -0300

    Comentarios Borrados

--- Unidad 6 Funciones/TP6_Funciones.py
+++ Unidad 6 Funciones/TP6_Funciones.py
@@ -2,10 +2,10 @@
 #pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
 #programa principal.
 
-#def imprimir_hola_mundo():
-#    print("Hola Mundo!")
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
 
-#imprimir_hola_mundo()
+imprimir_hola_mundo()
 
 
 #Ejercicio 2: Crear una función llamada saludar_usuario(nombre) que reciba
@@ -14,12 +14,12 @@
 #“Hola Marcos!”. Llamar a esta función desde el programa
 #principal solicitando el nombre al usuario.
 
-#def saludar_usuario(nombre):
-#    print(f"Hola {nombre_usuario}!")
+def saludar_usuario(nombre):
+    print(f"Hola {nombre_usuario}!")
 
-#nombre_usuario = input("Introduce tu nombre: ")
+nombre_usuario = input("Introduce tu nombre: ")
 
-#saludar_usuario(nombre_usuario)
+saludar_usuario(nombre_usuario)
 
 
 #Ejercicio 3: Crear una función llamada informacion_personal(nombre, apellido,
@@ -27,15 +27,15 @@
 #[nombre] [apellido], tengo [edad] años y vivo en [residencia]”. Pedir
 #los datos al usuario y llamar a esta función con los valores ingresados.
 
-#def informacion_personal(nombre, apellido, edad, residencia):
-#    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
-#nombre_usr = input("Introduce tu nombre de pila: ")
-#apellido_usr = input("Introduce tu primer apellido: ")
-#edad_usr = input("Introduce tu edad: ")
-#residencia_usr = input("Introduce tu lugar de residencia: ")
+nombre_usr = input("Introduce tu nombre de pila: ")
+apellido_usr = input("Introduce tu primer apellido: ")
+edad_usr = input("Introduce tu edad: ")
+residencia_usr = input("Introduce tu lugar de residencia: ")
 
-#informacion_personal(nombre_usr, apellido_usr, edad_usr, residencia_usr)
+informacion_personal(nombre_usr, apellido_usr, edad_usr, residencia_usr)
 
 
 #Ejercicio 4: Crear dos funciones: calcular_area_circulo(radio) que reciba el radio
@@ -44,16 +44,16 @@
 #el perímetro del círculo. Solicitar el radio al usuario y llamar ambas
 #funciones para mostrar los resultados.
 
-#def calcular_area_circulo(radio):
-#    return 3.14 * (radio ** 2)
+def calcular_area_circulo(radio):
+    return 3.14 * (radio ** 2)
 
-#def calcular_perimetro_circulo(radio):
-#    return (3.14 * 2) * radio
+def calcular_perimetro_circulo(radio):
+    return (3.14 * 2) * radio
 
-#radio = float(input("Escribe el radio del círculo: "))
+radio = float(input("Escribe el radio del círculo: "))
 
-#print(f"El área del círculo es {calcular_area_circulo(radio)}")
-#print(f"El perímetro del círculo es {calcular_perimetro_circulo(radio)}")
+print(f"El área del círculo es {calcular_area_circulo(radio)}")
+print(f"El perímetro del círculo es {calcular_perimetro_circulo(radio)}")
 
 
 #Ejercicio 5: Crear una función llamada segundos_a_horas(segundos) que reciba
@@ -61,26 +61,26 @@
 #de horas correspondientes. Solicitar al usuario los segundos y mostrar
 #el resultado usando esta función.
 
-#def segundos_a_horas(segundos):
-#    return segundos/3600
+def segundos_a_horas(segundos):
+    return segundos/3600
 
-#segundos_usr = int(input("Escriba la cantidad de segundos: "))
+segundos_usr = int(input("Escriba la cantidad de segundos: "))
 
-#print(f"{segundos_usr} equivalen a {segundos_a_horas(segundos_usr)} horas")
+print(f"{segundos_usr} equivalen a {segundos_a_horas(segundos_usr)} horas")
 
 
 #Ejercicio 6: Crear una función llamada tabla_multiplicar(numero) que reciba un
 #número como parámetro y imprima la tabla de multiplicar de ese
 #número del 1 al 10. Pedir al usuario el número y llamar a la función.
 
-#def tabla_de_multiplicar(numero):
-#    for i in range(11):
-#        producto = i * numero
-#        print (f"{numero} x {i} = {producto}")
+def tabla_de_multiplicar(numero):
+    for i in range(11):
+        producto = i * numero
+        print (f"{numero} x {i} = {producto}")
 
-#numero_usr = int(input("Escriba el número del que desea saber la tabla de multiplicar: "))
+numero_usr = int(input("Escriba el número del que desea saber la tabla de multiplicar: "))
 
-#tabla_de_multiplicar(numero_usr)
+tabla_de_multiplicar(numero_usr)
 
 
 #Ejercicio 7: Crear una función llamada operaciones_basicas(a, b) que reciba
@@ -88,16 +88,16 @@
 #de sumarlos, restarlos, multiplicarlos y dividirlos. Mostrar los resultados
 #de forma clara.
 
-#def operaciones_basicas(a, b):
-#    print(f"{a} + {b} es = {a + b}")
-#    print(f"{a} - {b} es = {a - b}")
-#    print(f"{a} x {b} es = {a * b}")
-#    print(f"{a} / {b} es = {a / b}")
+def operaciones_basicas(a, b):
+    print(f"{a} + {b} es = {a + b}")
+    print(f"{a} - {b} es = {a - b}")
+    print(f"{a} x {b} es = {a * b}")
+    print(f"{a} / {b} es = {a / b}")
 
-#numA = int(input("Escribe el primer número: "))
-#numB = int(input("Escribe el segundo número: "))    
+numA = int(input("Escribe el primer número: "))
+numB = int(input("Escribe el segundo número: "))    
 
-#operaciones_basicas(numA, numB)
+operaciones_basicas(numA, numB)
 
 
 #Ejercicio 8: Crear una función llamada calcular_imc(peso, altura) que reciba el
@@ -105,13 +105,13 @@
 #masa corporal (IMC). Solicitar al usuario los datos y llamar a la función
 #para mostrar el resultado con dos decimales.
 
-#def calcular_imc(peso, altura):
-#    return peso / (altura ** 2) 
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2) 
 
-#altura = float(input("Escribe tu altura expresada en metros (por ejemplo 1.75): "))
-#peso = float(input("Escribe tu peso expresado en kilogramos (por ejemplo 80): "))
+altura = float(input("Escribe tu altura expresada en metros (por ejemplo 1.75): "))
+peso = float(input("Escribe tu peso expresado en kilogramos (por ejemplo 80): "))
 
-#print(f"Tu IMC es {round(calcular_imc(peso, altura),2)}")
+print(f"Tu IMC es {round(calcular_imc(peso, altura),2)}")
 
 
 #Ejercicio 9: Crear una función llamada celsius_a_fahrenheit(celsius) que reciba
@@ -119,12 +119,12 @@
 #Fahrenheit. Pedir al usuario la temperatura en Celsius y mostrar el
 #resultado usando la función.
 
-#def celsius_a_fahrenheit(celsius):
-#    return (celsius * (9/5)) + 32
+def celsius_a_fahrenheit(celsius):
+    return (celsius * (9/5)) + 32
 
-#celsius = int(input("Escriba la cantidad de grados celcius que desea convertir a fahrenheit: "))
+celsius = int(input("Escriba la cantidad de grados celcius que desea convertir a fahrenheit: "))
 
-#print(f"{celsius}°C equivalen a {celsius_a_fahrenheit(celsius)}°F")
+print(f"{celsius}°C equivalen a {celsius_a_fahrenheit(celsius)}°F")
 
 
 #Ejercicio 10: Crear una función llamada calcular_promedio(a, b, c) que reciba
